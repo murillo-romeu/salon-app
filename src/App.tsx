f96b1d9f69c195fcf676571d775e0110a1c48b4d
Murillo ROmeu <murillosi@gmail.com>
2021-04-01 17:22:07 -0300

    iniciando primeiras páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import {View} from 'react-native';
+import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Routes from './routes';
 
-const App: React.FC = () => {
-  return <View />;
-};
+const App: React.FC = () => (
+  <NavigationContainer>
+    <StatusBar backgroundColor="#9B51E0" barStyle="light-content" />
+    <View style={{ backgroundColor: '#9B51E0', flex: 1 }}>
+      <Routes />
+    </View>
+  </NavigationContainer>
+);
 
 export default App;
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Image } from 'react-native';
+import { Container } from './styles';
+import logoImg from '../../assets/logo.png';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Image source={logoImg} />
+  </Container>
+);
+
+export default SignIn;
