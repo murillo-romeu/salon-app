5f258b9bb4373942c9b10ba74fdd320f42dc5440
Murillo ROmeu <murillosi@gmail.com>
2021-05-11 09:04:56 -0300

    Header da dashboard

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -5,9 +5,16 @@ import React, {
 import AsyncStorage from '@react-native-community/async-storage';
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  avatar_url: string;
+}
+
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 interface SignInCredentials {
@@ -16,7 +23,7 @@ interface SignInCredentials {
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   loading: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,36 @@
+import styled from 'styled-components/native';
+import { Platform } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  padding: 24px;
+  background: #28262e;
+  padding-top: ${Platform.OS === 'ios' ? getStatusBarHeight() : 0 + 24}px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const HeaderTitle = styled.Text`
+  color: #f4ede8;
+  font-size: 20px;
+  font-family: 'RobotoSlab-Reg';
+  line-height: 28px;
+`;
+
+export const UserName = styled.Text`
+  color: #9B51E0;
+  font-family: 'RobotoSlab-Medium';
+`;
+
+export const ProfileButton = styled.TouchableOpacity``;
+
+export const UserAvatar = styled.Image`
+  width: 56px;
+  height: 56px;
+  border-radius: 28px;
+`;
